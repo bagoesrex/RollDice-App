290747cc17fa626da70d9b1a66999da7e15e1257
bagoesrex <bagoesrex@gmail.com>
2025-03-01 02:44:21 +0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rolldice_app
+
+A fun and interactive Flutter app that simulates rolling dice. Tap to roll and get random results, perfect for games and decision-making!
---
+++ lib/styled_text.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// class StyledText extends StatelessWidget {
+//   const StyledText(this.text, {super.key});
+
+//   final String text;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       text,
+//       style: const TextStyle(
+//         color: Colors.greenAccent,
+//         fontSize: 28,
+//       ),
+//     );
+//   }
+// }
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Color.fromARGB(255, 255, 255, 255),
+        fontSize: 20,
+      ),
+    );
+  }
+}
